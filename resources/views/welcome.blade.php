8ee93db86eb7ee4c146dc96723e5541c44d8b5fd
Angga Risky <anggariskysetiawan@gmail.com>
2021-11-03 15:41:29 +0700

    new version

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>UI Design Resources by WillaStudio</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light pe-5 ps-5">
@@ -22,39 +22,146 @@
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
+                  Explore
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  <li><a class="dropdown-item" href="#">UI Kits</a></li>
+                  <li><a class="dropdown-item" href="#">Icon Sets</a></li>
                 </ul>
               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
             </ul>
-            <form class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <div class="d-flex">
+              <a href="#" class="btn btn-secondary me-2">Sign In</a>
+              <a href="#" class="btn btn-primary">Sign Up</a>
+            </div>
           </div>
         </div>
       </nav>
 
-      <section class="hero" style="margin-top: 200px;">
+      <section class="hero" style="margin-top: 100px;">
           <div class="container">
-              <div class="row">
-                  <div class="col-12 text-center">
-                      <h2>
-                          Coming Soon...
+              <div class="row justify-content-center">
+                  <div class="col-6 text-center">
+                      <h2 class="mb-4 lh-base">
+                        Working Faster and Easier <br class="desktop"> Using Our Product
                       </h2>
-                      <p>
-                          Follow our Instagram account for more update <a href="https://www.instagram.com/willastudiocom/">Willa Studio</a>
-                      </p>
+                      <form>
+                        <div class="mb-3">
+                          <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Search Finance or Education App" aria-describedby="emailHelp">
+                        </div>
+                      </form>
+                  </div>
+              </div>
+              <div class="row justify-content-center mt-5">
+                <div class="col-11">
+                    <div class="row">
+                        <div class="col-12">
+                            <h5 class="mb-3">
+                                Our Latest Products
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <a href="#">
+                                <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/844462/screenshots/14498339/media/873db5872fcef73c7dd7a491c6d096fc.png?compress=1&resize=1600x1200" alt="">
+                            </a>
+                        </div>
+                        <div class="col-4">
+                            <a href="#">
+                                <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/4712199/screenshots/11126503/media/115b6cad321b58c4dfbeee6f46091417.png?compress=1&resize=1600x1200" alt="">
+                            </a>
+                        </div>
+                        <div class="col-4">
+                            <a href="#">
+                                <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/844462/screenshots/14498339/media/873db5872fcef73c7dd7a491c6d096fc.png?compress=1&resize=1600x1200" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+              </div>
+          </div>
+      </section>
+
+      <section class="catalog mt-5 pb-5">
+          <div class="container">
+              <div class="row justify-content-center">
+                  <div class="col-11">
+                    <ul class="nav nav-tabs" id="myTab" role="tablist">
+                        <li class="nav-item" role="presentation">
+                          <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">All Assets</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                          <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">UI Kits</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                          <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Icon Sets</button>
+                        </li>
+                      </ul>
+                      <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <div class="row mt-3">
+                                <div class="col-3">
+                                    <a href="#">
+                                        <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/844462/screenshots/14498339/media/873db5872fcef73c7dd7a491c6d096fc.png?compress=1&resize=1600x1200" alt="">
+                                    </a>
+                                </div>
+                                <div class="col-3">
+                                    <a href="#">
+                                        <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/4712199/screenshots/11126503/media/115b6cad321b58c4dfbeee6f46091417.png?compress=1&resize=1600x1200" alt="">
+                                    </a>
+                                </div>
+                                <div class="col-3">
+                                    <a href="#">
+                                        <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/844462/screenshots/14498339/media/873db5872fcef73c7dd7a491c6d096fc.png?compress=1&resize=1600x1200" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class="row mt-3">
+                                <div class="col-3">
+                                    <a href="#">
+                                        <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/844462/screenshots/14498339/media/873db5872fcef73c7dd7a491c6d096fc.png?compress=1&resize=1600x1200" alt="">
+                                    </a>
+                                </div>
+                                <div class="col-3">
+                                    <a href="#">
+                                        <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/4712199/screenshots/11126503/media/115b6cad321b58c4dfbeee6f46091417.png?compress=1&resize=1600x1200" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                            <div class="row mt-3">
+                                <div class="col-3">
+                                    <a href="#">
+                                        <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/4712199/screenshots/11126503/media/115b6cad321b58c4dfbeee6f46091417.png?compress=1&resize=1600x1200" alt="">
+                                    </a>
+                                </div>
+                                <div class="col-3">
+                                    <a href="#">
+                                        <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/844462/screenshots/14498339/media/873db5872fcef73c7dd7a491c6d096fc.png?compress=1&resize=1600x1200" alt="">
+                                    </a>
+                                </div>
+                                <div class="col-3">
+                                    <a href="#">
+                                        <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/4712199/screenshots/11126503/media/115b6cad321b58c4dfbeee6f46091417.png?compress=1&resize=1600x1200" alt="">
+                                    </a>
+                                </div>
+                                <div class="col-3">
+                                    <a href="#">
+                                        <img class="img-fluid rounded" src="https://cdn.dribbble.com/users/4712199/screenshots/11126503/media/115b6cad321b58c4dfbeee6f46091417.png?compress=1&resize=1600x1200" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                      </div>
                   </div>
               </div>
           </div>
